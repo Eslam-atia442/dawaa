1349f50d79f4d27e3f6cad4ddd9924cc5c797975
Eslam Atia <56205474+Eslam-atia442@users.noreply.github.com>
2026-02-17 23:11:10 +0200

    Update BaseService.php

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -13,6 +13,7 @@ class BaseService
     public function __construct(BaseContract $repository)
     {
         $this->repository = $repository;
+        dd($this->repository);
     }
 
     public function search($filters = [], $relations = [], $data = [])
