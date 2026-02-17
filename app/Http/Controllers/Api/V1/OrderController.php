76b4e99703d5c28122189ad7fba5b3f9994eba03
Eslam Atia <56205474+Eslam-atia442@users.noreply.github.com>
2026-02-17 22:40:14 +0200

    Update OrderController.php

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -65,7 +65,6 @@ class OrderController extends Controller
 
         request()->merge(['myOrders' => true]);
         $relations = ['items', 'items.product', 'items.childProduct.parent'];
-        dd(request()->all());
         $orders = $this->orderService->search(request()->all(), $relations, []);
         return $this->respondWithArray(OrderResource::collection($orders));
     }
