5ec6491325a15bf5c86ef2b746444cedc08ff9fe
Eslam Atia <56205474+Eslam-atia442@users.noreply.github.com>
2026-03-01 00:08:49 +0200

    Update SetLocale.php

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -13,24 +13,15 @@ class SetLocale
     public function handle($request, Closure $next)
     {
         $lang = "ar";
-
-        if ($request->is('api/*')) {        
-                $lang = $request->header('accept-language');
-        } else {
-            if (Cookie::has('lang'))
-                $lang = Cookie::get('lang');
+        if (Cookie::has('lang'))
+            $lang = Cookie::get('lang');
 
 
-            if (
-                $request->header('Accept-Language')
-                && in_array($request->header('Accept-Language'), languages(),)
-            )
-                setcookie('lang', $request->header('Accept-Language'), time() + (86400 * 30), "/");
-        }
-       
-        if(!in_array($lang, languages())) {
-            $lang = 'ar';
-        }
+        if ($request->header('Accept-Language')
+            && in_array($request->header('Accept-Language'), languages(),))
+            setcookie('lang', $request->header('Accept-Language'), time() + (86400 * 30), "/");
+
+
         App::setLocale($lang);
         Carbon::setLocale($lang);
 
