b5b4d59a4e555dbfa174b2e160df71437b375bec
Eslam Atia <56205474+Eslam-atia442@users.noreply.github.com>
2026-03-01 00:07:27 +0200

    Update SetLocale.php

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -9,35 +9,24 @@ use Illuminate\Support\Facades\Cookie;
 
 class SetLocale
 {
+
     public function handle($request, Closure $next)
     {
-        $lang = 'ar';
+        $lang = "ar";
 
-        // ✅ Check if request URL starts with api/
-        if ($request->is('api/*')) {
-
-            $lang = $request->header('Accept-Language', 'ar');
-
+        if ($request->is('api/*')) {        
+                $lang = $request->header('accept-language');
         } else {
-
-            if (Cookie::has('lang')) {
+            if (Cookie::has('lang'))
                 $lang = Cookie::get('lang');
-            }
+
 
             if (
-                $request->header('Accept-Language') &&
-                in_array($request->header('Accept-Language'), languages())
-            ) {
-                Cookie::queue(
-                    'lang',
-                    $request->header('Accept-Language'),
-                    60 * 24 * 30
-                );
-
-                $lang = $request->header('Accept-Language');
-            }
+                $request->header('Accept-Language')
+                && in_array($request->header('Accept-Language'), languages(),)
+            )
+                setcookie('lang', $request->header('Accept-Language'), time() + (86400 * 30), "/");
         }
-
         App::setLocale($lang);
         Carbon::setLocale($lang);
 
